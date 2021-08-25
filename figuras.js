df6f9516e002e99cd047b8e21db7f6b8d9277f96
isaac953 <isaac.arias07@gmail.com>
2021-08-25 17:35:15 -0600

    Clase 6: Calculando figuras geométricas en JavaScript

--- figuras.js
+++ figuras.js
@@ -1,1 +1,70 @@
-console.log("Hello, World!");
+//Código del cuadrado
+
+console.group("Cuadrados");
+
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado + "cm^2");
+
+console.groupEnd();
+
+//Código del triangulo
+console.group("Triangulos")
+const ladoTriangulo1 = 6;
+
+const ladoTriangulo2 = 6;
+
+const baseTriangulo = 4;
+
+
+console.log(
+    "Los lados del triangulo miden: "
+    + ladoTriangulo1
+    + "cm, "
+    + ladoTriangulo2
+    + "cm, "
+    + baseTriangulo
+    + "cm"
+);
+
+const alturaTriangulo = 5.5;
+console.log("La altura del triángulo es de: " + alturaTriangulo);
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perimetro del triángulo es: " + perimetroTriangulo + "cm")
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El área del triángulo es: " + areaTriangulo + "cm^2");
+
+console.groupEnd();
+
+//Código del círculo
+console.group("Círculos");
+
+// Radio
+const radioCirculo = 4;
+console.log("El radio del circulo es: " + radioCirculo + "cm");
+
+// Diámetro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diámetro del circulo es: " + diametroCirculo + "cm");
+
+// Pi
+//const PI = 3.1415;
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log("El perímetro del círculo es: " + perimetroCirculo + "cm");
+
+// Área
+const areaCirculo = (diametroCirculo * radioCirculo) * PI;
+console.log("El área del círculo es: " + areaCirculo + "cm^2");
+
+console.groupEnd();
