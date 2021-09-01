8dc95cda64137f90a6a4dc557f31c0563b8a8bed
isaac953 <isaac.arias07@gmail.com>
2021-08-31 19:06:39 -0600

    Clase 12: Escribiendo HTML desde JavaScript

--- descuentos.js
+++ descuentos.js
@@ -17,3 +17,16 @@ calcularPrecioConDescuento(300, 20);
 //         precioConDescuento,
 //     }
 // )
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
