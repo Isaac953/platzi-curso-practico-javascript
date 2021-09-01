aad2ccf2adfe3df1997b6d709ce6e4e92d8d8a2b
isaac953 <isaac.arias07@gmail.com>
2021-09-01 17:45:30 -0600

    Clase 16: Calculando la mediana en Javascript (Reto)

--- mediana.js
+++ mediana.js
@@ -8,36 +8,49 @@ function calcularMediaAritmetica(lista) {
     return promedioLista;
 }
 
-const lista1 = [
-    100,
-    200,
-    500,
-    // 800,
-    400000000,
-];
+// const lista1 = [
+//     100,
+//     200,
+//     500,
+//     // 800,
+//     400000000,
+// ];
 
-const mitadLista1 = parseInt(lista1.length / 2);
+function calcularMediana(lista) {
 
-function esPar(numerito) {
-    if (numerito % 2 === 0) {
-        return true;
-    } else {
-        return false;
+    // const listaOrdenada = [5, 9, 10, 100, 5, 20];
+    const listaOrdenada = lista;
+    listaOrdenada.sort(function (a, b) {
+        return a - b;
+    });
+    console.log(listaOrdenada);
+
+    const mitadLista = parseInt(listaOrdenada.length / 2);
+
+    function esPar(numerito) {
+        if (numerito % 2 === 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
 
-let mediana;
+    let mediana;
 
-if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitadLista1 - 1];
-    const elemento2 = lista1[mitadLista1];
+    if (esPar(listaOrdenada.length)) {
+        const elemento1 = listaOrdenada[mitadLista - 1];
+        const elemento2 = listaOrdenada[mitadLista];
 
-    const promedioElemento1y2 = calcularMediaAritmetica([
-        elemento1,
-        elemento2,
-    ]);
+        const promedioElemento1y2 = calcularMediaAritmetica([
+            elemento1,
+            elemento2,
+        ]);
 
-    mediana = promedioElemento1y2;
-} else {
-    mediana = lista1[mitadLista1];
+        mediana = promedioElemento1y2;
+
+    } else {
+        mediana = listaOrdenada[mitadLista];
+
+    }
+    return mediana;
 }
